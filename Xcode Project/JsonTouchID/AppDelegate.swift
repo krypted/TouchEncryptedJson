3255c78ddb432724d65660e297a77030196bb39d
Charles Edge <krypted@me.com>
2022-02-11 12:18:11 -0600

    Update AppDelegate.swift

--- Xcode Project/JsonTouchID/AppDelegate.swift
+++ Xcode Project/JsonTouchID/AppDelegate.swift
@@ -2,15 +2,17 @@
 //  AppDelegate.swift
 //  JsonTouchID
 //  Uses RNCryptor to interface with the TouchID framework
+//  Meant as a PoC so InputA and InputB would be joined into a field in a json document
+//  The json document would then only be readable when TouchID was used to retrieve the ECC key
+//  Could also store the contents of that json document in a single field in user defaults or 
+//  instead use Core Storage/Keychain. However, for native mongo/nosql operations it is can be
+//  cheaper to keep them in natitve json.
 //
 
 import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    
-
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
